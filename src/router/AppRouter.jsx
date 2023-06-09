0f29014d39ab91191ce57701b056ef22d199f2de
David Toledo <dtoledoz0207@gmail.com>
2023-06-09 13:05:31 -0600

    Create a second route HeroesRoutes to show Navbar and heroes components and create SearchPage and HeroPage

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,22 +1,14 @@
-import { Navigate, Route, Routes } from 'react-router';
-import { DcPage, MarvelPage } from '../heroes';
+import { Route, Routes } from 'react-router';
+import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
-import { Navbar } from '../ui';
 
 
 export const AppRouter = () => {
   return(
     <>
-
-      <Navbar />
-
       <Routes>
-        <Route path='marvel' element={<MarvelPage />} />
-        <Route path='dc' element={<DcPage />} />
-
         <Route path='login' element={<LoginPage />} />
-
-        <Route path='/' element={<Navigate to='marvel' />} />
+        <Route path='/*' element={<HeroesRoutes />} />
       </Routes>
     </>
   );
