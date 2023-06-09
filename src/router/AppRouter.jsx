61565869e48b2ed97ad47d496f65a269914b8d97
David Toledo <dtoledoz0207@gmail.com>
2023-06-09 12:13:06 -0600

    Create Navbar Component and add index files to export componentes easily

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,15 @@
 import { Navigate, Route, Routes } from 'react-router';
-import { MarvelPage } from '../heroes/pages/MarvelPage';
-import { DcPage } from '../heroes/pages/DcPage';
-import { LoginPage } from '../auth/pages/LoginPage';
+import { DcPage, MarvelPage } from '../heroes';
+import { LoginPage } from '../auth';
+import { Navbar } from '../ui';
 
 
 export const AppRouter = () => {
   return(
     <>
+
+      <Navbar />
+
       <Routes>
         <Route path='marvel' element={<MarvelPage />} />
         <Route path='dc' element={<DcPage />} />
