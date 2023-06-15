69e77fccf73e103e18a5a4937ab3bd574f15856a
David Toledo <dtoledoz0207@gmail.com>
2023-06-15 11:07:04 -0600

    Add design to HeroPage

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,17 +1,42 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroeById } from "../helpers";
 
 export const HeroPage = () => {
 
-  const {id} = useParams();
+  const {id, ...rest} = useParams();
+  const navigate = useNavigate();
 
   const heroe = getHeroeById(id);
+
+  const onNavigateBack = () => {
+    navigate(-1)
+  }
 
   if (!heroe) {
     return <Navigate to="/marvel" />
   }
 
   return(
-    <h1>{heroe.superhero}</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={`/assets/heroes/${id}.jpg`}
+            alt={heroe.superhero}
+            className="img-thumbnail" />
+      </div>
+
+      <div className="col-8">
+        <h3>{heroe.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego:</b> {heroe.alter_ego}</li>
+          <li className="list-group-item"><b>Publisher:</b> {heroe.publisher}</li>
+          <li className="list-group-item"><b>First appearance:</b> {heroe.first_appearance}</li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{heroe.characters}</p>
+
+        <button className="btn btn-outline-primary" onClick={onNavigateBack}>Back</button>
+      </div>
+    </div>
   );
 }
