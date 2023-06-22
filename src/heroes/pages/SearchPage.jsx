2e1dd107da554efe47e1738db6b58202ad6abeec
David Toledo <dtoledoz0207@gmail.com>
2023-06-22 11:42:46 -0600

    Add design for SearchPage

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,44 @@
+import { HeroeCard } from '../components';
 
 export const SearchPage = () => {
   return(
-    <h1>SearchPage</h1>
+    <>
+      <h1>SearchPage</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+
+          <form>
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+
+            <button className="btn btn-outline-primary mt-1">Search</button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-primary">
+            Search a hero
+          </div>
+
+          <div className="alert alert-danger">
+            No hero found
+          </div>
+
+          {/* <HeroeCard /> */}
+        </div>
+      </div>
+    </>
   );
 }
