4ad6a713fad30519b833e27f666620e41f7f9c51
David Toledo <dtoledoz0207@gmail.com>
2023-06-23 10:48:44 -0600

    Show conditional alerts in SearchPage

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -19,10 +19,14 @@ export const SearchPage = () => {
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length <= 1) return;
+    // if (searchText.trim().length <= 1) return;
 
     navigate(`?q=${searchText}`);
   }
+
+
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0 && heroes.length === 0);
 
   return(
     <>
@@ -53,11 +57,11 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
+          <div className="alert alert-primary animate__animated animate__fadeIn" style={{display: showSearch ? '' : 'none'}}>
             Search a hero
           </div>
 
-          <div className="alert alert-danger">
+          <div className="alert alert-danger animate__animated animate__fadeIn" style={{display: showError ? '' : 'none'}}>
             No hero found ({q})
           </div>
 
