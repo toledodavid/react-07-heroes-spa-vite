f713335c0dadb837382965e0c44a6f9105b2acc6
David Toledo <dtoledoz0207@gmail.com>
2023-06-28 13:03:57 -0600

    Implement localStorage in AuthProvider to keep user logged

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,22 +4,32 @@ import { authReducer } from './authReducer';
 import { types } from '../types/types';
 
 
-const initialState = {
-  logged: false,
-};
+
+const init = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  return {
+    logged: !!user,
+    user
+  };
+}
 
 export const AuthProvider = ({children}) => {
 
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, {}, init);
 
   const login = (name = '') => {
+
+    const user = {
+      id: '123',
+      name
+    };
+
     const action = {
       type: types.login,
-      payload: {
-        id: '123',
-        name
-      }
+      payload: user
     };
+
+    localStorage.setItem('user', JSON.stringify(user));
 
     dispatch(action);
   }
