512389d752b0a7255824aec66a74fe78dabd9380
David Toledo <dtoledoz0207@gmail.com>
2023-06-28 12:52:28 -0600

    Implement login functionality with AuthContext

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { useReducer } from 'react';
 import { AuthContext } from './AuthContext';
 import { authReducer } from './authReducer';
+import { types } from '../types/types';
 
 
 const initialState = {
@@ -11,8 +12,20 @@ export const AuthProvider = ({children}) => {
 
   const [authState, dispatch] = useReducer(authReducer, initialState);
 
+  const login = (name = '') => {
+    const action = {
+      type: types.login,
+      payload: {
+        id: '123',
+        name
+      }
+    };
+
+    dispatch(action);
+  }
+
   return(
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{...authState, login}}>
       {children}
     </AuthContext.Provider>
   );
