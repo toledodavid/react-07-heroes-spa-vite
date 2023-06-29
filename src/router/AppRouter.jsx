bdfc46edbf14349de2db3d407a3f2ef21d6daffe
David Toledo <dtoledoz0207@gmail.com>
2023-06-29 11:40:09 -0600

    Create PublicRoute component

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,18 @@ import { Route, Routes } from 'react-router';
 import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 
 export const AppRouter = () => {
   return(
     <>
       <Routes>
-        <Route path='login' element={<LoginPage />} />
+        <Route path='login' element={
+        <PublicRoute>
+          <LoginPage />
+        </PublicRoute>
+      } />
 
         <Route path='/*' element={
           <PrivateRoute>
