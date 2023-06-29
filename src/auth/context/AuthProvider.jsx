056a366791b559c7861b4cfb6e12ae23c92ec98c
David Toledo <dtoledoz0207@gmail.com>
2023-06-29 10:38:54 -0600

    Implement logout functionality

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -34,8 +34,17 @@ export const AuthProvider = ({children}) => {
     dispatch(action);
   }
 
+  const logout = () => {
+    const action = {
+      type: types.logout
+    }
+
+    localStorage.removeItem('user');
+    dispatch(action);
+  }
+
   return(
-    <AuthContext.Provider value={{...authState, login}}>
+    <AuthContext.Provider value={{...authState, login, logout}}>
       {children}
     </AuthContext.Provider>
   );
