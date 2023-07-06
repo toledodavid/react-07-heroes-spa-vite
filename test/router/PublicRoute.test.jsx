8fa33131b875464d455390f5374daeb32faac952
David Toledo <dtoledoz0207@gmail.com>
2023-07-06 13:25:29 -0600

    Create a second test for PublicRoute component when user is logged

--- test/router/PublicRoute.test.jsx
+++ test/router/PublicRoute.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { AuthContext } from '../../src/auth/context';
 import { PublicRoute } from '../../src/router/PublicRoute';
 
@@ -22,7 +23,33 @@ describe('Test for <PublicRoute />', () => {
   });
 
   test('should navigate if user is logged', () => {
+    const contextValue = {
+      logged: true,
+      user: {
+        name: 'David',
+        id: '123'
+      }
+    }
 
+    render(
+      <AuthContext.Provider value={contextValue}>
+
+        <MemoryRouter initialEntries={['/login']}>
+          <Routes>
+            <Route path='login' element={
+              <PublicRoute>
+                <h1>Public route</h1>
+              </PublicRoute>
+            }/>
+
+            <Route path='/' element={<h1>Mavel page</h1>} />
+          </Routes>
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+
+    // screen.debug();
+    expect(screen.getByText('Mavel page')).toBeTruthy();
   });
 
 });
