f7f58f0b418f7c9cbaad43a4d12a80e44f7811ed
David Toledo <dtoledoz0207@gmail.com>
2023-07-17 12:36:43 -0600

    Create test for queryString in SearchPage component

--- test/heroes/pages/SearchPage.test.jsx
+++ test/heroes/pages/SearchPage.test.jsx
@@ -16,4 +16,21 @@ describe('Tests for <SearchPage />', () => {
     expect(container).toMatchSnapshot();
   });
 
+  test('should show Batman and input element with value from queryString', () => {
+    render(
+      <MemoryRouter initialEntries={['/search?q=batman']}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+
+    screen.debug();
+    const input = screen.getByRole('textbox');
+    const img = screen.getByRole('img');
+    const notFoundAlert = screen.getByLabelText('alert-danger');
+
+    expect(input.value).toBe('batman');
+    expect(img.src).toContain('/assets/heroes/dc-batman.jpg');
+    expect(notFoundAlert.style.display).toBe('none');
+  });
+
 });
